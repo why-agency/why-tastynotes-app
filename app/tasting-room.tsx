36db839ec88cff641e007ff14526277b86a4f24f
Jan Mayer <jan.demmerle@why.de>
2023-12-15 18:16:13 +0100

    feat: add confetti animation

--- app/tasting-room.tsx
+++ app/tasting-room.tsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from 'react'
 import { createClient } from '@/utils/supabase/client'
 import Link from 'next/link'
 
+import { Canvas } from '@react-three/fiber'
+import Confetti from './confetti'
+
 interface TastingNote {
   id: number
   title: string
@@ -19,6 +22,8 @@ export default function TastingRoom({
 }: {
   serverTastingNotes: TastingNote[]
 }) {
+  const [isConfettiActive, setIsConfettiActive] = useState(false)
+
   const [tastingNotes, setTastingNotes] =
     useState<TastingNote[]>(serverTastingNotes)
 
@@ -34,6 +39,10 @@ export default function TastingRoom({
         },
         payload => {
           console.log('channelNotes payload', payload, tastingNotes)
+          setIsConfettiActive(true)
+          setTimeout(() => {
+            setIsConfettiActive(false)
+          }, 1000)
           return setTastingNotes(
             tastingNotes.concat(payload.new as TastingNote)
           )
@@ -73,32 +82,43 @@ export default function TastingRoom({
     )
 
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full">
-      {tastingNotes.map((tastingNote, index) => {
-        const [background, foreground, text] = getCardColors(index)
-        return (
-          <li
-            key={tastingNote.id}
-            className={`flex justify-center items-center w-full min-h-[480px] 2xl:min-h-[600px] ${background} ${text}`}
-          >
-            <div
-              className={`w-[240px] h-[255px] xl:w-[280px] xl:h-[288px] 2xl:w-[320px] 2xl:h-[364px] rounded-xl p-6 shadow-2xl ${foreground} flex flex-col justify-between`}
+    <>
+      {isConfettiActive && (
+        <Canvas
+          style={{ position: 'absolute' }}
+          className="absolute inset-0 w-full h-full pointer-events-none z-10 bg-transparent"
+        >
+          {/* @ts-ignore */}
+          <Confetti isExploding position={[0, 0, 3]} />
+        </Canvas>
+      )}
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full">
+        {tastingNotes.map((tastingNote, index) => {
+          const [background, foreground, text] = getCardColors(index)
+          return (
+            <li
+              key={tastingNote.id}
+              className={`flex justify-center items-center w-full min-h-[480px] 2xl:min-h-[600px] ${background} ${text}`}
             >
-              <div>
-                <h2 className="text-[32px] font-secondary font-light">
-                  {tastingNote.title}
-                </h2>
-                <p className="text-sm text-[15px] opacity-80">
-                  Tasted by {tastingNote.created_by}
+              <div
+                className={`w-[240px] h-[255px] xl:w-[280px] xl:h-[288px] 2xl:w-[320px] 2xl:h-[364px] rounded-xl p-6 shadow-2xl ${foreground} flex flex-col justify-between`}
+              >
+                <div>
+                  <h2 className="text-[32px] font-secondary font-light">
+                    {tastingNote.title}
+                  </h2>
+                  <p className="text-sm text-[15px] opacity-80">
+                    Tasted by {tastingNote.created_by}
+                  </p>
+                </div>
+                <p className="text-sm text-[15px] opacity-80 line-clamp-2">
+                  {tastingNote.note}
                 </p>
               </div>
-              <p className="text-sm text-[15px] opacity-80 line-clamp-2">
-                {tastingNote.note}
-              </p>
-            </div>
-          </li>
-        )
-      })}
-    </ul>
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
